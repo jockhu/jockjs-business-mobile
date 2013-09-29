862c1155f326e7713210683f4eca2ffbc53915b0
胡家友 <jockhu@anjuke.com>
2013-09-29 17:51:28 +0800

    modify city to refresh

--- touch/touch.js
+++ touch/touch.js
@@ -79,7 +79,7 @@ J.add('touch');
         }
 
         w.onpopstate = function(event){
-            if(J.site.info.ctid==14||J.site.info.ctid==12||J.site.info.ctid==11) return false;
+            //if(J.site.info.ctid==14||J.site.info.ctid==12||J.site.info.ctid==11) return false;
             enableTransition = false;
             var pn, currentPage, cubPage ,hsPage;
             event.state&&(pn=event.state.pageName);
@@ -614,7 +614,7 @@ J.add('touch');
             function remove(){
                 historyPage = null;
             }
-            if(history.pushState&&(J.site.info.ctid!=14&&J.site.info.ctid!=12&&J.site.info.ctid!=11)){
+            if(history.pushState/*&&(J.site.info.ctid!=14&&J.site.info.ctid!=12&&J.site.info.ctid!=11)*/){
                 historyPage = options;
                 var back=function(callback){
                     history.back();
@@ -917,7 +917,7 @@ J.add('touch');
 
     function isSupported(){
        // alert(J.site.info.ctid); hanhui
-        if(J.site.info.ctid==14||J.site.info.ctid==12) return false;
+        //if(J.site.info.ctid==14||J.site.info.ctid==12) return false;
         return  isSupportHistory && history.pushState;
     }
 
