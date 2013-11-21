ee280d5c6293b85e25b513c21f72742530ff391e
胡家友 <jockhu@anjuke.com>
2013-11-21 11:23:54 +0800

    优化ppc发送判断

--- site/init.js
+++ site/init.js
@@ -47,12 +47,12 @@
         p.cityAlias && (site.info.cityAlias = p.cityAlias);
         p.includePrefix && (site.info.includePrefix = p.includePrefix);
 
-        var head = D.head || D.getElementsByTagName( "head" )[0], pageName = head.getAttribute('data-page'),testflag=head.getAttribute("data-testflag"),datappc=head.getAttribute("data-ppc");
+        var head = D.head || D.getElementsByTagName( "head" )[0], pageName = head.getAttribute('data-page'),testflag=head.getAttribute("data-testflag"),pageppc=head.getAttribute("data-ppc");
 
         if(pageName){
             site.tracked = true;
             site.info.pageName = pageName;
-            (new Image()).src = datappc;
+            pageppc && ((new Image()).src = pageppc);
             J.logger.trackEvent({site:'m_anjuke', page:pageName, customparam: '{"refresh":"1","TH":"1","testflag":"'+testflag+'"}'});
         }
         site.setRef();
